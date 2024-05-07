18ea3c1fefaec46ba5387540d72fb179c040caf9
Macbook Air-Caesario Dito <caesariodito@gmail.com>
2024-05-07 09:29:05 +0700

    initialize locevent project

---
+++ locevent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  locevent
+//
+//  Created by MacBook Air on 07/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Something")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ locevent/loceventApp.swift
@@ -0,0 +1,18 @@
+//
+//  loceventApp.swift
+//  locevent
+//
+//  Created by MacBook Air on 07/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct loceventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
