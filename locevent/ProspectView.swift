5af172494832fd5af6a8261c3a3633017b451661
Macbook Air-Caesario Dito <caesariodito@gmail.com>
2024-05-07 11:20:03 +0700

    feat: add search with api

--- locevent/ProspectView.swift
+++ locevent/ProspectView.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 
+struct Message: Identifiable, Codable {
+    let id: Int
+    var user: String
+    var text: String
+}
+
+enum SearchScope: String, CaseIterable {
+    case inbox, favorites
+}
+
 struct ProspectView: View {
-    let dummies = ["Fak", "Yea", "iloveyou", "Ted"]
+    @State private var messages = [Message]()
     
     @State private var searchText = ""
+    @State private var searchScope = SearchScope.inbox
     @State private var searchIsActive = false
     
     enum FilterType {
@@ -31,11 +42,11 @@ struct ProspectView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(searchResults, id: \.self) { dummy in
-                    NavigationLink {
-                        Text(dummy)
-                    } label: {
-                        Text(dummy)
+                ForEach(filteredMessages) { message in
+                    VStack(alignment: .leading) {
+                        Text(message.user)
+                            .font(.headline)
+                        Text(message.text)
                     }
                 }
             }
@@ -43,13 +54,30 @@ struct ProspectView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
         .searchable(text: $searchText, isPresented: $searchIsActive, prompt: "find events!")
+        .searchScopes($searchScope) {
+            ForEach(SearchScope.allCases, id: \.self) { scope in
+                Text(scope.rawValue.capitalized)
+            }
+        }
+        .onAppear(perform: runSearch)
+        .onSubmit(of: .search, runSearch)
+        .onChange(of: searchScope) { runSearch() }
     }
     
-    var searchResults: [String] {
+    var filteredMessages: [Message] {
         if searchText.isEmpty {
-            return dummies
+            return messages
         } else {
-            return dummies.filter { $0.contains(searchText) }
+            return messages.filter { $0.text.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    func runSearch() {
+        Task {
+            guard let url = URL(string: "https://hws.dev/\(searchScope.rawValue).json") else { return }
+            
+            let (data, _) = try await URLSession.shared.data(from: url)
+            messages = try JSONDecoder().decode([Message].self, from: data)
         }
     }
 }
